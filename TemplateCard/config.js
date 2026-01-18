a2c882ec45b93a58fa6463720cdcaa5555a644ed
Egael <egaelvalle@gmail.com>
2026-01-18 01:32:31 -0600

    Add files via upload

---
+++ TemplateCard/config.js
@@ -0,0 +1,31 @@
+const tarjeta = {
+    nombre: "Nombre",
+    descripcion: "Descripción larga que habla sobre ti, tus servicios, tu experiencia y lo que quieras compartir",
+    foto: "img/perfil.png",
+    portada: "img/portada.jpg",
+    telefono: "0000000000",
+    correo: "tucorreo",
+
+  redes: [
+    {
+      nombre: "WhatsApp",
+      link: "https://wa.me/",
+      icono: "https://cdn-icons-png.flaticon.com/128/3670/3670051.png"
+    },
+    {
+      nombre: "Instagram",
+      link: "https://www.instagram.com/",
+      icono: "https://cdn-icons-png.flaticon.com/128/4494/4494488.png"
+    },
+    {
+      nombre: "Facebook",
+      link: "https://www.facebook.com/",
+      icono: "https://cdn-icons-png.flaticon.com/128/5968/5968764.png"
+    },
+    {
+        nombre: "TikTok",
+        link: "https://www.tiktok.com/@",
+        icono: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWXXatvGdUncxQEmUwic8sz5sOGJCfT-ES2A&s"
+    }
+  ]
+};
