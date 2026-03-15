ee6856ae8678036064fbf24beb3f606d63fa10e8
Egael <egaelvalle@gmail.com>
2026-03-15 01:26:53 -0600

    nueva creación

---
+++ card/config.js
@@ -0,0 +1,26 @@
+const tarjeta = {
+    nombre: "Capa Zero",
+    descripcion: "Objetos funcionales, ideas originales y soluciones <br>creativas para cualquier persona<br><br>No imprimimos plástico  <br>Imprimimos ideas",
+    foto: "img/perfil.png",
+    portada: "img/portada.jpg",
+    telefono: "5664122581",
+    correo: "capazero.3d@protonmail.com",
+
+  redes: [
+    {
+      nombre: "WhatsApp",
+      link: "https://wa.me/524921298921",
+      icono: "https://cdn-icons-png.flaticon.com/128/3670/3670051.png"
+    },
+    {
+      nombre: "Instagram",
+      link: "https://www.instagram.com/capazero.3d",
+      icono: "https://cdn-icons-png.flaticon.com/128/4494/4494488.png"
+    },
+    {
+      nombre: "Facebook",
+      link: "https://www.facebook.com/capazero3d",
+      icono: "https://cdn-icons-png.flaticon.com/128/5968/5968764.png"
+    }
+  ]
+};
